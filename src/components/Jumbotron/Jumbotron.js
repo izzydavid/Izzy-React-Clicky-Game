f6d3d8e7fa09aba8cfdadb4f28b79de1f1027ccd
IzzyDavid <david.irizarry86@yahoo.com>
2019-10-17 20:42:35 -0400

    fixing

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">React Clicky Game!</h1>
+    <p className="lead">Click on any image to earn points, but don't click on any more than once!</p>
+  </div>
+</div>
+);
+
+export default Jumbotron;
